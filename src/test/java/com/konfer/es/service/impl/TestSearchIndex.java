eaa29550746bbae458960731bdb109bf08d070c5
yuanlin <ly_kof@sina.com>
2018-03-06 01:11:36 +0800

    search update

--- src/test/java/com/konfer/es/service/impl/TestSearchIndex.java
+++ src/test/java/com/konfer/es/service/impl/TestSearchIndex.java
@@ -2,7 +2,13 @@ package com.konfer.es.service.impl;
 
 import Util.Conn.EsClusterConn.ClusterConn;
 import Util.Conn.EsClusterConn.TestUnitConfiguration;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.transport.TransportClient;
+
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,6 +16,7 @@ public class TestSearchIndex
 {
     private TransportClient client = null;
     private SearchIndexServiceImpl searchIndexService = null;
+    private QueryBuilders queryBuilders;
 
     @Before
     public void testConn()
@@ -20,6 +27,8 @@ public class TestSearchIndex
         searchIndexService=new SearchIndexServiceImpl();
         searchIndexService.setIndex(TestUnitConfiguration.testIndex);
         searchIndexService.setIndexType(TestUnitConfiguration.testIndexType);
+
+
     }
 
     @Test
@@ -27,6 +36,26 @@ public class TestSearchIndex
     {
         long indexNum=searchIndexService.getSearchResponse().getHits().getTotalHits();
         System.out.println("The num of index is: "+indexNum);
+    }
+
+    @Test
+    public void testSearchByMatchQuery() throws Exception
+    {
+        SearchResponse searchResponse=searchIndexService.getSearchRequestBuilder()
+                .setQuery(QueryBuilders.matchQuery("name","lin"))
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .get();
+        SearchHits hits=searchResponse.getHits();
+        long searchIndexNum=hits.getTotalHits();
+        System.out.println("found index num is: "+searchIndexNum);
+
+        SearchHit[] searchHits=hits.getHits();
+        for (SearchHit searchHit:searchHits)
+        {
+            System.out.println(searchHit.getSourceAsString());
+        }
+
+
     }
 
 
